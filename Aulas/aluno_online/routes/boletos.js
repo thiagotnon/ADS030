aa8f0fbc6cb94af793a4cb02be332a9cd052b91b
Thiago Nascimento <=>
2020-10-16 16:19:21 -0300

    Autenticação de páginas

--- Aulas/aluno_online/routes/boletos.js
+++ Aulas/aluno_online/routes/boletos.js
@@ -2,6 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
+  if (req.session.alunoLogado == null) {
+    return res.redirect('/login');
+  }
   res.render('boletos', {title: 'Boletos - Aluno Online'});
 });
 
